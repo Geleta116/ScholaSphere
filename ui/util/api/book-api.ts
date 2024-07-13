cebd18a7b688759a7d66f2fd1c4566715ad6451d
Geleta116 <geleta.dabakumsa@gmail.com>
2024-07-13 07:49:31 +0300

    started the book api

--- ui/util/api/book-api.ts
+++ ui/util/api/book-api.ts
@@ -1,3 +1,10 @@
-import {api} from "./shared";
+import { api, bookPath } from "./shared";
 
-
+export interface CreateBookPayload {
+  title: string;
+  description: string;
+  tags: string[];
+  file: File;
+  year: number;
+  department: string;
+}
