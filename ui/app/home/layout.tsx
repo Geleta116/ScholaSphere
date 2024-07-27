d529b3ca3996a8d5857df4de91d87b96d208a4f4
Geleta116 <geleta.dabakumsa@gmail.com>
2024-07-27 11:48:36 +0300

    fixed background color issue

--- ui/app/home/layout.tsx
+++ ui/app/home/layout.tsx
@@ -6,14 +6,16 @@ export default function HomeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex-1   ">
-         <div className=" fixed inset-0 dark:bg-black bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1]"></div>
-            <div className=" fixed inset-0 pointer-events-none flex items-center justify-center dark:bg-black-800 bg-black [mask-image:radial-gradient(ellipse_at_top,transparent_90%,black)]"></div>
-            <div className=" fixed inset-0 pointer-events-none flex items-center justify-center dark:bg-purple-800 bg-black [mask-image:radial-gradient(ellipse_at_top,transparent_70%,black)]"></div>
-      <NavBar />
+    <div>
+      <div className="fixed inset-0 z-10 dark:bg-black bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1]"></div>
+      <div className="fixed inset-0 z-20 pointer-events-none flex items-center justify-center dark:bg-black-800 bg-black [mask-image:radial-gradient(ellipse_at_top,transparent_90%,black)]"></div>
+      <div className="fixed inset-0 z-30 pointer-events-none flex items-center justify-center dark:bg-purple-800 bg-black [mask-image:radial-gradient(ellipse_at_top,transparent_70%,black)]"></div>
+      <div className="relative z-40">
+        <NavBar />
 
-      {children}
+        {children}
       </div>
+    </div>
     // </div>
   );
 }
